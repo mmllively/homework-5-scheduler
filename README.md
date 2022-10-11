1abffcee0e9d05f1f95f53e522b7b888d99ac67f
Maggie Lively <maggielively@maggielvelyspro.lan>
2022-10-11 15:04:11 -0400

    getItem fix

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # homework-5-scheduler
 Scheduling App for your Work Day
+
+## Description
+
+This quiz tests my ability to use JavaScript, jQuery and localStorage to create a scheduler.
+
+https://mmllively.github.io/homework-5-scheduler/
+![alt text](./Screen%20Shot%202022-10-10%20at%205.16.51%20PM.png)
+
+## Credits
+
+All of the tutors, TAs and fellow UCF Bootcamp students.
--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var timeBlock = [
   "hour-17",
 ];
 
-function func() {
+function funcTime() {
   //Extract "hour-" from each string in the array
 
   for (let index = 0; index < timeBlock.length; index++) {
@@ -39,10 +39,10 @@ document.getElementById("hour-" + newTimeLength).classList.add("future");
   }
 }
 }
-func();
+funcTime();
 
 var buttonInput = document.getElementsByClassName("saveBtn");
-console.log(buttonInput);
+// console.log(buttonInput);
 for (let index = 0; index < buttonInput.length; index++) {
   buttonInput[index].addEventListener("click", inputForm);
   
@@ -56,50 +56,53 @@ console.log("click")
  let hour9 = $("#hour-9").val();
  console.log(hour9);
   localStorage.setItem("hour-9", hour9);
-  $("#hour-9").val(localStorage.getItem("hour-9"));
+ 
+  console.log("click")
+ let hour10 = $("#hour-10").val();
+ console.log(hour10);
+  localStorage.setItem("hour-10", hour10);
+  
+  console.log("click")
+ let hour11 = $("#hour-11").val();
+ console.log(hour11);
+  localStorage.setItem("hour-11", hour11);
+  
+  console.log("click")
+ let hour12 = $("#hour-12").val();
+ console.log(hour12);
+  localStorage.setItem("hour-12", hour12);
+  
+  console.log("click")
+ let hour13 = $("#hour-13").val();
+ console.log(hour13);
+  localStorage.setItem("hour-13", hour13);
 
-  let hour10 = $("#hour-10").val();
-  console.log(hour10);
-   localStorage.setItem("hour-10", hour10);
-   $("#hour-10").val(localStorage.getItem("hour-10"));
+  console.log("click")
+ let hour14 = $("#hour-14").val();
+ console.log(hour14);
+  localStorage.setItem("hour-14", hour14);
 
-   let hour11 = $("#hour-11").val();
-   console.log(hour11);
-    localStorage.setItem("hour-11", hour11);
-    $("#hour-11").val(localStorage.getItem("hour-11"));
+  console.log("click")
+ let hour15 = $("#hour-15").val();
+ console.log(hour15);
+  localStorage.setItem("hour-15", hour15);
+  
+  console.log("click")
+  let hour16 = $("#hour-16").val();
+  console.log(hour16);
+   localStorage.setItem("hour-16", hour16);
+   
+   console.log("click")
+   let hour17 = $("#hour-17").val();
+   console.log(hour17);
+    localStorage.setItem("hour-17", hour17);
 
-    let hour12 = $("#hour-12").val();
-    console.log(hour12);
-     localStorage.setItem("hour-12", hour12);
-     $("#hour-12").val(localStorage.getItem("hour-12"));
-
-     let hour13 = $("#hour-13").val();
-     console.log(hour13);
-      localStorage.setItem("hour-13", hour13);
-      $("#hour-13").val(localStorage.getItem("hour-13"));
-
-      let hour14 = $("#hour-14").val();
-      console.log(hour14);
-       localStorage.setItem("hour-14", hour14);
-       $("#hour-14").val(localStorage.getItem("hour-14"));
 
       //  WHEN I refresh the page
       //  THEN the saved events persist
-
+      
 }
  
-  // let hour10 = $("hour-10").val();
-  // localStorage.setItem("hour-10", hour10);
-  // $("hour-10").val(localStorage.getItem("hour-10"));
-  // let hour11 = $("hour-11").val();
-  // localStorage.setItem("hour-11", hour11);
-  // $("hour-11").val(localStorage.getItem("hour-11"));
-  // let hour12 = $("hour-12").val();
-  // localStorage.setItem("hour-12", hour12);
-  // $("hour-12").val(localStorage.getItem("hour-21"));
-
-
-
 
 
 
@@ -113,3 +116,15 @@ console.log("click")
 // - each div is given an id = "hour-9" split the id, take out the hour- and then see if 9 is greater than or less than to give the class past or class future
 // - onclick jquery
 // - jquery getting the value of the form
+
+
+
+$("#hour-9").val(localStorage.getItem("hour-9"));
+$("#hour-10").val(localStorage.getItem("hour-10"));
+$("#hour-11").val(localStorage.getItem("hour-11"));
+$("#hour-12").val(localStorage.getItem("hour-12"));
+$("#hour-13").val(localStorage.getItem("hour-13"));
+$("#hour-14").val(localStorage.getItem("hour-14"));
+$("#hour-15").val(localStorage.getItem("hour-15"));
+$("#hour-16").val(localStorage.getItem("hour-16"));
+$("#hour-17").val(localStorage.getItem("hour-17"));
